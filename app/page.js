5376dea311e8254b1d2faf02ef2c701358771f27
xfibben <eyck.arturo@icloud.com>
2023-09-09 15:53:22 -0500

    lsslslsl

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
-import Image from 'next/image'
 
+import Suma from "@/app/components/suma";
+import Menu from "@/app/components/menus";
 export default function Home() {
-  return (
-    <main>
-      <h1>CHatbot title</h1>
-    </main>
-  )
+
+
+    return (
+        <main className={'w-full grid'}>
+            <Menu/>
+            <h1 className={'mx-auto text-6xl'}>Home</h1>
+            <Suma x={parseInt(5)} y={parseInt(10)}> </Suma>
+        </main>
+
+    )
 }
