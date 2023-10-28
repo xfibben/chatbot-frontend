a7a7e6c8276802b56608115165c45563f670400c
xfibben <eyck.arturo@icloud.com>
2023-10-28 17:43:18 -0500

    start to create the chatbot

--- app/chatbot.jsx
+++ app/chatbot.jsx
@@ -8,11 +8,12 @@ const Chatbot = () => {
 
   const toggleChatbot = () => {
     setChatbotVisible(!chatbotVisible);
-    setButtonVisible(!buttonVisible)
+    setButtonVisible(buttonVisible)
   };
 
   return (
     <div className="chatbot-container">
+
       {buttonVisible && (<img
         src="ruta-de-tu-imagen-pequena.png"
         alt="Activar Chatbot"
@@ -21,10 +22,24 @@ const Chatbot = () => {
       )}
 
       {chatbotVisible && (
-        <div className="chatbot">
+        <div className="m-4 border-gray-900 bg-gray-200 h-full w-full">
           {/* Aquí puedes integrar la lógica de tu chatbot */}
-          <p>Bienvenido al chatbot</p>
+          <p className='text-center'>Bienvenido al chatbot</p>
           {/* Añade la lógica de respuesta del chatbot aquí */}
+          <div>
+            <div>
+              <h1>Netrada</h1>
+
+            </div>
+            <div>
+              <h1 className='ml-auto mr-0 '>Salida</h1>
+
+            </div>
+
+            <div>
+              <input></input>
+            </div>
+          </div>
         </div>
       )}
 
